e3767849f7b34e66cbf8c1611401ac3c0df12473
Matt Neary <neary.matt@gmail.com>
2013-08-04 01:55:17 -0400

    bootstrap needs compatibility with SLanguage

--- src/code/bootstrap.lisp
+++ src/code/bootstrap.lisp
@@ -1,13 +1,14 @@
 (load "src/Prelude.lampas")
 
-" TODO: handle multiple passed arguments
+"  TODO: handle multiple passed arguments.
+"" TODO: `atom?` is not actually implemented.
 "
 (letrec eval (lambda (eval expr env) 
   (cond 
     (((atom? expr) (assocv expr env)) 
-    ((eqv? (car expr) 'lambda) 
+    ((equal? (car expr) 'lambda) 
      (lambda (x) 5))
     ((atom? (car expr)) (eval (map (lambda (expr) (eval expr env)) expr) env))
-    ((eqv? (car (car expr)) 'lambda) 
+    ((equal? (car (car expr)) 'lambda) 
      ((lambda (argname) (eval (car (cdr (cdr (car expr)))) (set argname (cadr expr) env))) (car (car (cdr (car expr)))))))))
   (eval '((lambda (x) (x 2)) (lambda (x) x)) '((2 2))))
